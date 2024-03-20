4b48701fb332995f34ac097a301b11d99bfe38c2
namsan01 <namsan2934@gmail.com>
2024-03-20 11:22:26 +0900

    Graphql Apollo Server 스터디

--- database.js
+++ database.js
@@ -1,8 +1,11 @@
+// 실제 DB 활용시 교체 대상
 const csvToJson = require("convert-csv-to-json");
 
+// 위에서 불러들인 csv 내용을 보관할 배열
 const database = {
   todos: [],
 };
+// 객체의 key를 파악해서 반복하는 코드
 Object.keys(database).forEach((key) => {
   database[key] = [
     ...database[key],
